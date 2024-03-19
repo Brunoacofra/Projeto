04f42a9981ec19fef433f754b2d90da755a97c5d
Brunoacofra <doalreeu@gmail.com>
2024-03-19 10:40:08 -0300

    manutenção

--- Functions.js
+++ Functions.js
@@ -5,6 +5,11 @@ let mensagem = document.getElementById('mensagem')
 let email = document.getElementById('email')
 var form = document.querySelector('btn');
 let msg = document.getElementsByClassName("texto")
+var home = document.getElementById("linkahome")
+var contato = document.getElementById("linkacontato")
+var campos = document.getElementById("campos")
+var homediv = document.getElementById("home")
+var p = document.getElementById("campinho")
 function nameverifica(){
 	if (name.value == 0){
 		name.setAttribute("style" , "border-color:red")
@@ -34,6 +39,16 @@ function mensagemverifica(){
 		mensagem.setAttribute("style" ,  "border-color:green")
 		msg[2].setAttribute("style","display:none")
 	}
+}
+home.onclick = function (){
+	campos.setAttribute("style","display:none")
+	p.setAttribute("style","display:none")
+	homediv.setAttribute("style","display:block")
+}
+contato.onclick = function (){
+	homediv.setAttribute("style","display:none")
+	campos.setAttribute("style","display:block")
+	p.setAttribute("style","display:block")
 }
 btn.onclick = function (){
 	nameverifica();
